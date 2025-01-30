9f56b1c3bc8e511a42f98ab16301c205f438048c
ABHINAV KUMAR <114585720+ABHINAVKUMAR011@users.noreply.github.com>
2025-01-31 01:23:29 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+const account_id = 12345
+let account_gmail = "abhinav34@gmail.com"
+var account_pass = "234567"
+account_city = "muz"
+console.log(account_id)
+console.log(account_pass)
+console.table([account_gmail,account_id,account_pass,account_city])
---
+++ 02_datatype.js
@@ -0,0 +1,16 @@
+"use strict"; //it used for treat code as updated and newer version of node js
+//Alter (3+3) //we are using node js , not browser or connection with html
+console.log(3+3);
+console.log("abhinav");
+// ensure code redability
+//number = it is used for integer
+//bigint = it is used for long int
+//string="" it is used for storing string
+//boolean = true/false
+//null standalone value null is a type
+//undefined when a variable is assigned and it is not defined;
+// symbols it is used for unique
+// object
+console.log(typeof("hitesh"))
+console.log(typeof null)
+console.log(typeof undefined)
